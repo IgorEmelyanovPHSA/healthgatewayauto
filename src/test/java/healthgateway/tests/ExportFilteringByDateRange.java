f2c3adfb4b45759ae80fb35c0a7694aa5fe88817
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2024-09-26 14:34:48 -0700

    qax: fixes for ExportFilteringDataByDateRange

--- src/test/java/healthgateway/tests/ExportFilteringByDateRange.java
+++ src/test/java/healthgateway/tests/ExportFilteringByDateRange.java
@@ -20,31 +20,38 @@ public class ExportFilteringByDateRange extends BaseTest {
 	private String textSearchEndDate = "Jun 1, 2024";
 
 	@Test
-	public void ExportFilteringDataByDateRange() throws Exception {
+	public void Can_Filtering_Export_data_by_Date_Range() throws Exception {
 		TestcaseID = "322313"; //Original TC C322313
 		log("Target Environment: " + Utils.getTargetEnvironment());
 		log("Target Browser: " + Utils.getTargetBrowser());
 		log("Test Case " +"C" +TestcaseID);
 
 		//Login as user 11
+		log("/*1.---Login as user 11 ---*/");
 		MainPageHealthGateway mainPageHealthGateway = loginPage.loginIntoHGWithBCServiceCardAsUser11();
 
 		//GoTo Export tab
+		log("/*2.---GoTo Export tab ---*/");
 		ExportTabPage export = mainPageHealthGateway.goToTabExport();
 
 		//Immunization dropDown must be selected first
+		log("/*3.---Immunization dropDown must be selected first ---*/");
 		export.filterByImmunization();
 
 		//Search by the date range
+		log("/*4.---Search by the date range ---*/");
 		export.filterByStartAndEndDate(textSearchStartDate, textSearchEndDate);
 
 		//Validate number of records matched by date range
+		log("/*5.---Validate number of records matched by date range ---*/");
 		Assert.assertTrue( export.getNumberOfRecords() == numberOfRecordsExpectedForDateRange, "Number of records displayed after date range search didn't match!");
 
 		//Remove search results
+		log("/*6.---Remove search results ---*/");
 		export.removeSearchResults();
 
 		//Validate total count of records
+		log("/*7.---Validate total count of records ---*/");
 		Assert.assertTrue(export.getNumberOfRecords() == numberOfAllRecords, "Total number of all records displayed didn't match!");
 
 		log("Filtering using the search box pass successfully");
